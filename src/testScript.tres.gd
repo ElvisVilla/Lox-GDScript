a956bb75394782b08e35c8982fe1d72223bd4d25
Bissash <bissash05@gmail.com>
2025-09-27 22:23:05 +0200

    Reorganize forlder

--- src/testScript.tres.gd
+++ src/testScript.tres.gd
@@ -5,8 +5,3 @@ var source = "Hello world"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 		Lox.main("res://TestFile.gd")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
